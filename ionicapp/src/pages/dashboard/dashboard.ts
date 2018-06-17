36388972dbf81d7b030904ef08cc6fecd7dba29e
Eduardo Miranda Soares <eduardo@heds.com.br>
2018-06-17 08:27:08 -0300

    ajuste build produção

--- ionicapp/src/pages/dashboard/dashboard.ts
+++ ionicapp/src/pages/dashboard/dashboard.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { PiqueteApi, FinanceiroMovimentacaoApi } from '../../app/shared/sdk';
 
-@IonicPage()
+//@IonicPage()
 @Component({
   selector: 'page-dashboard',
   templateUrl: 'dashboard.html',
