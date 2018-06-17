f22c553c6b1077277bc584798958325be1c2a4c6
Marcus Veloso <desenvolvedoroficial@gmail.com>
2018-06-16 22:02:21 -0300

    desenvolvedoroficial@gmail.com

--- ionicapp/src/pages/financeiro-movimentacao-pagar-form/financeiro-movimentacao-pagar-form.ts
+++ ionicapp/src/pages/financeiro-movimentacao-pagar-form/financeiro-movimentacao-pagar-form.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { FinanceiroMovimentacao, FinanceiroMovimentacaoApi } from '../../app/shared/sdk';
+import { FinanceiroMovimentacao, FinanceiroMovimentacaoApi, FinanceiroCategoriaApi, FinanceiroCategoria } from '../../app/shared/sdk';
 
 @IonicPage()
 @Component({
@@ -9,16 +9,23 @@ import { FinanceiroMovimentacao, FinanceiroMovimentacaoApi } from '../../app/sha
 })
 export class FinanceiroMovimentacaoPagarFormPage {
 
+  listaCategorias: FinanceiroCategoria[];
   public dadosDoForm: FinanceiroMovimentacao = new FinanceiroMovimentacao();
 
   constructor(public navCtrl: NavController, 
       public navParams: NavParams,
-      public API: FinanceiroMovimentacaoApi) {
+      public API: FinanceiroMovimentacaoApi,
+      public categoriaApi: FinanceiroCategoriaApi) {
     let item = navParams.get('item');
     if (item) this.dadosDoForm = Object.assign(new FinanceiroMovimentacao, item);
   }
 
   ionViewDidLoad() {
+    //Buscar Todos as Categorias
+    this.categoriaApi.find().subscribe(
+      (retorno: FinanceiroCategoria[]) => {
+        this.listaCategorias = retorno;
+      })
   }
 
   salvar() {
