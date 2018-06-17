3b9a4d3de012576fa8b0e3b22f5cfd1b045a2eed
Hudson <hduson@huds.com.br>
2018-06-16 22:26:45 -0300

    Primeira versão do lançamento dos eventos dos animais

--- ionicapp/src/pages/animal-eventos-form/animal-eventos-form.ts
+++ ionicapp/src/pages/animal-eventos-form/animal-eventos-form.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the AnimalEventosFormPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { AnimalEventos, Animal, AnimalEventosApi } from '../../app/shared/sdk';
 
 @IonicPage()
 @Component({
@@ -15,11 +9,66 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AnimalEventosFormPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public dadosDoForm: AnimalEventos = new AnimalEventos();
+  public piquete: Animal = null;
+  public callback: any = null;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public API: AnimalEventosApi,
+    public viewCtrl: ViewController
+  ) {
+    let _item = navParams.get('item');
+    if (_item) this.dadosDoForm = Object.assign(new AnimalEventos, _item);
+    let _piquete = navParams.get('piquete');
+    if (_piquete) this.piquete = Object.assign(new AnimalEventos, _piquete);
+    let _callback = navParams.get('callback');
+    if (_callback) this.callback = _callback;
+
+    this.dadosDoForm.tipo = 'vacina';
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AnimalEventosFormPage');
+  salvar() {
+
+    try {
+      if (!this.dadosDoForm.nome) throw 'Informe uma nome do evento';
+      if (!this.dadosDoForm.tipo) throw 'Informe o tipo de evento';
+
+      if (this.piquete) {
+        this.dadosDoForm.animalId = this.piquete.id;
+      }
+
+      this.API.upsert(this.dadosDoForm).subscribe(
+        (data: AnimalEventos) => {
+          if (this.callback) {
+            this.callback().then(() => { this.viewCtrl.dismiss().catch(() => {}); });
+          }
+          this.navCtrl.pop();
+        }
+      )
+
+    } catch (error) {
+      alert(error);
+    }
+  }
+
+  excluir() {
+    if (confirm('deseja realmente excluir?')) {
+      this.API.deleteById(this.dadosDoForm.id).subscribe(
+        () => {
+          if (this.callback) {
+            this.callback().then(() => { this.viewCtrl.dismiss(); });
+          }else{
+            this.navCtrl.pop();
+          }
+        }
+      );
+    }
+  }
+
+  fechar(){
+    this.navCtrl.pop();
   }
 
 }
--- ionicapp/src/pages/animal-form/animal-form.ts
+++ ionicapp/src/pages/animal-form/animal-form.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { Animal, AnimalApi } from '../../app/shared/sdk';
+import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
+import { Animal, AnimalApi, AnimalEventos, AnimalEventosApi } from '../../app/shared/sdk';
 
 @IonicPage()
 @Component({
@@ -10,15 +10,48 @@ import { Animal, AnimalApi } from '../../app/shared/sdk';
 export class AnimalFormPage {
 
   public dadosDoForm: Animal = new Animal();
+  public listaEventos: AnimalEventos[] = [];
+  public animalSegment: string = 'informacoes';
 
-  constructor(public navCtrl: NavController, 
-    public navParams: NavParams, public API: AnimalApi) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public API: AnimalApi,
+    public animalEventosApi: AnimalEventosApi,
+    public modalCtrl: ModalController
+  ) {
     let item = navParams.get('item');
     if (item) this.dadosDoForm = Object.assign(new Animal, item);
+
+    this.dadosDoForm.raca = "nelore"
+    this.dadosDoForm.tipo = "bovino";
+    this.dadosDoForm.finalidade = "cria";
+    this.dadosDoForm.status = "vivo";
+    this.dadosDoForm.unidadeAnimal = 1;
+    this.dadosDoForm.sexo = false;
+    this.dadosDoForm.categoria = "remover";
   }
 
-  ionViewDidLoad() {
+  ionViewDidEnter() {
+    this.buscarEventos();
+  }
 
+  buscarEventos() {
+    this.animalEventosApi.find({ where: { piqueteId: this.dadosDoForm.id }, order: 'createdAt DESC' }).subscribe(
+      (data: AnimalEventos[]) => {
+        this.listaEventos = data;
+      }
+    )
+  }
+
+  abrirEvento(evento: AnimalEventos) {
+    let modal = this.modalCtrl.create('AnimalEventosFormPage', { item: evento, callback: this.callbackEventos });
+    modal.present();
+  }
+
+  novoEvento() {
+    let modal = this.modalCtrl.create('AnimalEventosFormPage', { animal: this.dadosDoForm, callback: this.callbackEventos });
+    modal.present();
   }
 
   salvar() {
@@ -40,5 +73,13 @@ export class AnimalFormPage {
   excluir() {
 
   }
+
+  callbackEventos = (_params) => {
+    return new Promise((resolve, reject) => {
+      this.buscarEventos();
+      resolve();
+    })
+  }
+
 
 }
